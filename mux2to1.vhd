6a493dd9c96999b3e4fb456a0ad7a59dbb1004a2
Michelangelo98 <s245478@studenti.polito.it>
2020-03-19 19:43:40 +0100

    Prima versione multiplexer 2to1

---
+++ mux2to1.vhd
@@ -0,0 +1,15 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+
+--simple multiplexer with 2 inputs
+ENTITY mux2to1 IS
+PORT ( x,y : IN STD_LOGIC;	--inputs
+		 s : IN STD_LOGIC;	--select signal
+		 m : OUT STD_LOGIC);
+END mux2to1;
+
+--logical structure of a multiplexer
+ARCHITECTURE logic OF mux2to1 IS
+BEGIN
+	m <= (NOT (s) AND x) OR (s AND y);
+END ARCHITECTURE;
