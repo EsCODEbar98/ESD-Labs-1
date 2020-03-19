954f160806c43fd4869e145087b4c6bbd369a252
LawlietElle <Massimiliano.Pronesti@gmail.com>
2020-03-19 22:55:01 +0100

    forgot semicolon

--- mux2to1.vhd
+++ mux2to1.vhd
@@ -1,14 +1,14 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.all;
 
---simple multiplexer with 2 inputs
+--simple multiplexer with 2 inputs
 ENTITY mux2to1 IS
-PORT ( x,y : IN STD_LOGIC;	--inputs
-		 s : IN STD_LOGIC;	--select signal
+PORT ( x,y : IN STD_LOGIC;	
+		 s : IN STD_LOGIC;	
 		 m : OUT STD_LOGIC);
 END mux2to1;
 
---logical structure of a multiplexer
+
 ARCHITECTURE logic OF mux2to1 IS
 BEGIN
 	m <= (NOT (s) AND x) OR (s AND y);
