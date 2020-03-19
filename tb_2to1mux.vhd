6c70fbf6f9aee13ef8acc28399f6f695a972c931
Mariavittoria-oropallo <62213006+Mariavittoria-oropallo@users.noreply.github.com>
2020-03-19 19:15:12 +0100

    correzioni

--- tb_2to1mux.vhd
+++ tb_2to1mux.vhd
@@ -1,57 +1,49 @@
-library ieee;
-use ieee.std_logic_1164.all;
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
 
-entity tb_mux2to1 is
-end tb_mux2to1;
+ENTITY tb_mux2to1 IS
+END tb_mux2to1;
 
-architecture behavior of tb_mux2to1 is
+ARCHITECTURE Behavior OF tb_mux2to1 IS
   
   --component employed in this circuit
-  component mux2to1 is
-  port (
+  COMPONENT mux2to1
+  PORT (
         x, y : in std_logic_vector(7 downto 0);
         s : in std_logic;
         m : out std_logic_vector(7 downto 0)
       );
-  end component;
+  END COMPONENT;
     
-  --declaration of singnals employed in this circuit
-  signal x : std_logic_vector(7 downto 0);
-  signal y : std_logic_vector(7 downto 0);
-  signal s : std_logic;
-  signal m : std_logic_vector(7 downto 0);
+  --Testbench signals
   
-  begin
+  SIGNAL x : std_logic_vector(7 downto 0);
+  SIGNAL y : std_logic_vector(7 downto 0);
+  SIGNAL s : std_logic;
+  SIGNAL m : std_logic_vector(7 downto 0);
+  
+  BEGIN
     -- association between signals and ports
-    port map (
+    PORT MAP (
       x => x,
       y => y,
       s => s,
       m => m
     );
     
+    x <= "00000000";
+    y <= "11111111";
+    
     -- stimulus process
     PROCESS
-      begin
-        
-        wait for 20 ns;
-        
-        x <= "00001111";
-        y <= "11110000";
+      BEGIN     
         s <= '0';
-        
-        wait for 20 ns;
-        
-        x <= "00000000";
-        y <= "11111111";
+        WAIT FOT 20 ns;
         s <= '1';
-        
-        wait;
-      end PROCESS;
-      
-    end;
+        WAIT FOR 20 ns;
+      END PROCESS;
     
-  end architecture;
+  END Behavior;
         
   
        
