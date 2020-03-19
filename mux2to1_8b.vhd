84e8ead25f19f9492874ca7c3b1b9bda741093a9
LawlietElle <Massimiliano.Pronesti@gmail.com>
2020-03-19 22:52:14 +0100

    added 8bit wide 2to1 mux

---
+++ mux2to1_8b.vhd
@@ -0,0 +1,26 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+
+ENTITY mux2to1_8b is
+PORT (
+     X : in STD_LOGIC_VECTOR ( 7 downto 0 );
+     Y : in STD_LOGIC_VECTOR ( 7 downto 0 );
+     S : in STD_LOGIC;
+     M : out STD_LOGIC_VECTOR ( 7 downto 0 );
+     )
+END mux2to1_8b;
+
+ARCHITECTURE Behavioral of mux2to1_8b is
+BEGIN
+     -- 8 statements explicitly required 
+     M(7) <= (NOT(S) AND X(7)) OR (S AND Y(7));  
+     M(6) <= (NOT(S) AND X(6)) OR (S AND Y(6));
+     M(5) <= (NOT(S) AND X(5)) OR (S AND Y(5));
+     M(4) <= (NOT(S) AND X(4)) OR (S AND Y(4));	
+     M(3) <= (NOT(S) AND X(3)) OR (S AND Y(3));
+     M(2) <= (NOT(S) AND X(2)) OR (S AND Y(2));
+     M(1) <= (NOT(S) AND X(1)) OR (S AND Y(1));
+     M(0) <= (NOT(S) AND X(0)) OR (S AND Y(0));
+END Behavioral
+
+
--- mux5to1.vhd
+++ mux5to1.vhd
@@ -1,24 +1,24 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.all;
 
---Multiplexer with 5 inputs and 3 selected signal
+--Multiplexer with 5 inputs and 3 selection signals
 
 ENTITY mux5to1 IS
-	PORT ( u,v,w,x,y : IN STD_LOGIC; 				--inputs
-			 s : IN STD_LOGIC_VECTOR (2 downto 0); --selcted signals
-			 m : OUT STD_LOGIC );						--output
+	PORT ( u,v,w,x,y : IN STD_LOGIC; 				
+			 s : IN STD_LOGIC_VECTOR (2 downto 0); 
+			 m : OUT STD_LOGIC 
+            );						
 END mux5to1;
 
---I build a mux5to1 using 4 mux 2to1
+--mux5to1 using 4 mux 2to1
 ARCHITECTURE mux_structure OF mux5to1 IS
-SIGNAL m_uv,m_wx,m_uv_wx : STD_LOGIC ; 	--I need some intermediate signal to connect muxes
-COMPONENT mux2to1									--Declaration of the component that will be used
+SIGNAL m_uv,m_wx,m_uv_wx : STD_LOGIC ; 	
+COMPONENT mux2to1									
 	PORT ( in_1,in_2 : IN STD_LOGIC;	
 		 s : IN STD_LOGIC;	
 		 m : OUT STD_LOGIC);
 END COMPONENT;
 BEGIN
-	--The scheme of the connections is more clear in the report
 	Mux1 : mux2to1 PORT MAP ( in_1 => u , in_2 => v , s => s(0) , m => m_uv );
 	Mux2 : mux2to1 PORT MAP ( in_1 => w , in_2 => x , s => s(0) , m => m_wx );
 	Mux3 : mux2to1 PORT MAP ( in_1 => m_uv , in_2 => m_wx , s => s(1) , m => m_uv_wx );
