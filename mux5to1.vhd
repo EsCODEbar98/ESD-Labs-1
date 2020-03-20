3f8a0cb914240996f5d6ecb02faf45ebb1930cb1
Riki-code98 <62204561+Riki-code98@users.noreply.github.com>
2020-03-20 17:46:11 +0100

    Errore riga 20
    Gli IN del mux5to1 sono dichiarati diversamente rispetto alla dichiarazione del mux2to1

--- mux5to1.vhd
+++ mux5to1.vhd
@@ -4,21 +4,21 @@ use ieee.std_logic_1164.all;
 --Multiplexer with 5 inputs and 3 selection signals
 
 entity mux5to1 is
-port ( 
-       u,v,w,x,y : in STD_LOGIC; 				
-       s : in STD_LOGIC_VECTOR (2 downto 0); 
-       m : out STD_LOGIC 
-      );						
+port (
+       u,v,w,x,y : in STD_LOGIC;
+       s : in STD_LOGIC_VECTOR (2 downto 0);
+       m : out STD_LOGIC
+      );
 end mux5to1;
 
 --mux5to1 using 4 mux 2to1
 architecture mux_structure of mux5to1 is
-signal m_uv,m_wx,m_uv_wx : STD_LOGIC ; 	
+signal m_uv,m_wx,m_uv_wx : STD_LOGIC ;
 
-component mux2to1									
-port ( 
-     in_1,in_2 : in STD_LOGIC;	
-     s : in STD_LOGIC;	
+component mux2to1
+port (
+     x,y : in STD_LOGIC;	
+     s : in STD_LOGIC;
      m : out STD_LOGIC
      );
 end component;
@@ -28,6 +28,5 @@ begin
 	MUX2 : mux2to1 port map ( in_1 => w , in_2 => x , s => s(0) , m => m_wx );
 	MUX3 : mux2to1 port map ( in_1 => m_uv , in_2 => m_wx , s => s(1) , m => m_uv_wx );
 	MUX4 : mux2to1 port map ( in_1 => m_uv_wx , in_2 => y , s => s(2) , m => m );
-	
-end mux_structure;
 
+end mux_structure;
