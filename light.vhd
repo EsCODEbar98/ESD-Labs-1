1b77550bf2415072bfc71137ab2e16f4e29a1039
Elle <44113430+PronElle@users.noreply.github.com>
2020-03-21 22:15:41 +0100

    refactoring

--- light.vhd
+++ light.vhd
@@ -4,11 +4,13 @@ use ieee.std_logic_1164.all;
 -- Simple module that connects the SW switches to the LEDR lights
 
 entity light is
- port (SW : in std_logic_vector(17 downto 0);
-   LEDR : out std_logic_vector(17 downto 0)); --red LEDs
+ port (
+     SW : in std_logic_vector(17 downto 0);
+     LEDR : out std_logic_vector(17 downto 0) --red LEDs
+ ); 
 end light;
 
 architecture behavior of light is
 begin
- LEDR <= SW; --LED assignment
+ LEDR <= SW;
 end behavior;
