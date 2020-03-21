9026c8349ca7611021af378da96fce4d544927fd
LawlietElle <Massimiliano.Pronesti@gmail.com>
2020-03-21 16:51:31 +0100

    added working 5to1mux_tb! Be aware on USAGE

--- mux5to1_tb.vhd
+++ mux5to1_tb.vhd
@@ -1,38 +1,56 @@
+-- USAGE: in the "Objects" scope, select ONLY m_dut and m_ref 
+--        in order to compare the expected output and the obtained one!
+
+
 library ieee;
 use ieee.std_logic_1164.all;
 
---NO SIGNAL in the entity section it's a test
+-- empty entity section
 entity tb_mux5to1 is
 end tb_mux5to1;
 
 architecture Behavioral of tb_mux5to1 is
---declare the componenet to be employed in this circuit
-component mux5to1 is
-  port (  
-       u,v,w,x,y : in std_logic;
-       s : in std_logic_vector( 2 downto 0);
-       m  : out std_logic
-  );
-end component;
-  
---declare the signals to be employed in this circuit
- signal inputstring : std_logic_vector( 2 DOWNTO 0) ; 
- signal u_dut,v_dut,w_dut,x_dut,y_dut : std_logic;--inputs DEVICE UNDER TEST
- signal u_ref,v_ref,w_ref,x_ref,y_ref : std_logic;-- reference inputs
- signal m_dut : std_logic; --output DEVICE UNDER TEST
- signal m_ref : std_logic; -- reference output
- 
- begin
-   --time evolution of three input signals
-   inputstring <= "000","001" after 5 ns, "010" after 10 ns,"011" after 15 ns,"100" after 20 ns,"101" after 25 ns,"110" after 30 ns,"111" after 35 ns;
-  --port mapping for the test
-  DUT: mux5to1 port map (s=> inputstring, m=>m_dut,u=>u_dut,v=>v_dut,w=>w_dut,x=>x_dut,y=>y_dut);
 
---reference process
-with inputstring select
-m_ref<=  u_ref when "000",
-        v_ref  when "001",
-        w_ref when "010",
-        x_ref when "011",
-        y_ref when others ;
+-- testing signals
+
+signal u_dut, v_dut, w_dut, x_dut, y_dut, m_dut : std_logic;
+signal sel : std_logic_vector (2 downto 0) ;
+signal m_ref: std_logic; -- reference (expected) output 
+
+begin
+
+-- implicit process testcase
+
+sel <= "000",
+       "001" after 5 ns, 
+       "010" after 10 ns,
+       "011" after 15 ns,
+       "100" after 20 ns,
+       "101" after 25 ns,
+       "100" after 30 ns,
+       "111" after 35 ns;
+
+-- example input
+
+    u_dut <= '1';
+    v_dut <= '0';
+    w_dut <= '0';
+    x_dut <= '1';
+    y_dut <= '1';
+
+
+-- testing instance
+   DUT : entity work.mux5to1 port map (u_dut, v_dut, w_dut, x_dut, y_dut, sel, m_dut);
+
+
+-- expected output def
+
+with sel select 
+     m_ref <= u_dut when "000",
+              v_dut when "001",
+              w_dut when "010",
+              x_dut when "011",
+              y_dut when others;
+
+  
 end architecture;
