a8b4112d1172ef9e62ae5e64fc3d45a4d308a074
Michelangelo98 <48916321+Michelangelo98@users.noreply.github.com>
2020-03-23 12:10:31 +0100

    Aggiunta di qualche commento

--- mux2to1_gen_tb.vhd
+++ mux2to1_gen_tb.vhd
@@ -1,16 +1,24 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
+--Tb per un multiplexer 2to1 con parallelismo generico
+--Di default lavora con mux ad un bit, ma basya cambiare il parametro p e l'assegnazione dei valori
+--ai segnali di ingresso e funziona per qualsiasi parallelismo
+
 entity mux2to1_gen_tb is
 generic ( p : POSITIVE := 1);
 end mux2to1_gen_tb;
 
 architecture test of mux2to1_gen_tb is
+  
+  ---- segnali test
   signal in1 : STD_LOGIC_VECTOR ( (p-1)  downto 0);
   signal in2 : STD_LOGIC_VECTOR ( (p-1) downto 0);
-  signal selectchar : STD_LOGIC;
+  signal sel : STD_LOGIC;
   signal m_dut, m_ref : STD_LOGIC_VECTOR ((p-1) downto 0);
   
+
+  --utilizzo un mux2to1 generico come component da testare
   component mux2to1_gen
   generic (p : POSITIVE := p);
   port ( 
@@ -20,15 +28,20 @@ architecture test of mux2to1_gen_tb is
       );
   end component;
 begin
+  
+  --assegnamento valori arbitrari agli ingressi
   in1 <= "0";
   in2 <= "1";
-  selectchar <= '0', '1' after 5 ns;
-  dut: mux2to1_gen port map( x => in1, y => in2, s => selectchar, m => m_dut);
+
+ 
+  sel <= '0', '1' after 5 ns;
+  dut: mux2to1_gen port map( x => in1, y => in2, s => sel, m => m_dut);
     
-with selectchar select
-m_ref <=
-  in2 when '1',
-  in1 when others;
+    
+  with sel select
+  m_ref <=
+    in2 when '1',
+    in1 when others;
   
   
 end test;
