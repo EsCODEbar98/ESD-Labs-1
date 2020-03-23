08f2a3048424297c662608ee15fe09672aa93cc5
Michelangelo98 <48916321+Michelangelo98@users.noreply.github.com>
2020-03-23 12:02:24 +0100

    mux2to1 commentato

--- mux2to1_gen.vhd
+++ mux2to1_gen.vhd
@@ -1,9 +1,9 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
---simple multiplexer with 2 inputs
+--Multiplexer con  2 ingressi e parallelismo p
 entity mux2to1_gen is
-generic ( p : POSITIVE := 1 );
+generic ( p : POSITIVE := 1 );   --si imposta 1 come valore di defaoult per il parallelismo  
 port ( 
        x,y : in STD_LOGIC_VECTOR( (p-1) downto 0 );	
        s : in STD_LOGIC;	
@@ -13,10 +13,10 @@ end mux2to1_gen;
 
 
 architecture logic of mux2to1_gen is
-  
-  signal s_vector : STD_LOGIC_VECTOR ((p-1) downto 0 );
+ 
+  signal s_vector : STD_LOGIC_VECTOR ((p-1) downto 0 );     --segnale vettoriale in cui ciascun bit è uguale a s
   
 begin
   s_vector <= ( others => s ); 
-	m <= (NOT (s_vector) AND x) OR (s_vector AND y);
+	m <= (NOT (s_vector) AND x) OR (s_vector AND y);   -- affinchè non si abbia errore di tipo bisogna usare s_vector, non s
 end architecture;
