993474b3a0c0aac49a2dfe4c778b7156a8ff8532
Mariavittoria-oropallo <62213006+Mariavittoria-oropallo@users.noreply.github.com>
2020-03-26 21:00:35 +0100

    Esercizio 1

---
+++ decoder.vhd
@@ -0,0 +1,22 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity decoder is
+  port ( c : in std_logic_vector(2 downto 0);
+        HEX0 : out std_logic_vector(0 to 6));
+end decoder;
+
+architecture behavior of decoder is
+  begin
+  -- boolean functions 
+  HEX0(0) <= not(c(0) and (not c(2)));
+  HEX0(1) <= c(2) or (not(((not c(1)) and c(0)) or (c(1) and c(0))));
+  HEX0(2) <= c(2) or (not(((not c(1)) and c(0)) or (c(1) and c(0))));
+  HEX0(3) <= c(2) or (not(c(0) or c(1)));
+  HEX0(4) <= c(2);
+  HEX0(5) <= c(2);
+  HEX0(6) <= (c(1) or c(2));
+  
+  --the light switches on when the driven value is '0'
+   
+end behavior; 
---
+++ decoder_tb.vhd
@@ -0,0 +1,27 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity decoder_tb is
+end decoder_tb;
+
+architecture test of decoder_tb is
+  
+  signal sel : std_logic_vector(2 downto 0);
+  signal output : std_logic_vector(0 to 6);
+  signal out_ref : string(2 downto 1);
+  
+  begin
+    --assigned values to c2,c1,c0
+    sel <= "000", "001" after 10 ns, "010" after 20 ns, "011" after 30 ns, "100" after 40 ns;
+    DUT: entity work.decoder port map (c => sel, HEX0 => output);
+    
+    --expected output
+    with sel select 
+     out_ref <= 
+      "h " when "000",
+      "e " when "001",
+      "l " when "010",
+      "o " when "011",
+      "x " when others;
+      
+  end test;
