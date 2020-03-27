aa104aa22ce127bd08d27228bad24b7407d53463
Riki-code98 <62204561+Riki-code98@users.noreply.github.com>
2020-03-27 18:52:47 +0100

    Funzionante
    Modifica espressioni booleane

--- decoder.vhd
+++ decoder.vhd
@@ -8,15 +8,15 @@ end decoder;
 
 architecture behavior of decoder is
   begin
-  -- boolean functions 
-  HEX0(0) <= not(c(0) and (not c(2)));
-  HEX0(1) <= c(2) or (not(((not c(1)) and c(0)) or (c(1) and c(0))));
-  HEX0(2) <= c(2) or (not(((not c(1)) and c(0)) or (c(1) and c(0))));
+  -- boolean functions
+  HEX0(0) <= not(c(0)) or c(2);
+  HEX0(1) <= c(2) or (c(0) and not(c(1))) or (not(c(0)) and c(1));
+  HEX0(2) <= c(2) or (c(0) and not(c(1))) or (not(c(0)) and c(1)) ;
   HEX0(3) <= c(2) or (not(c(0) or c(1)));
   HEX0(4) <= c(2);
   HEX0(5) <= c(2);
   HEX0(6) <= (c(1) or c(2));
-  
+
   --the light switches on when the driven value is '0'
-   
-end behavior; 
+
+end behavior;
