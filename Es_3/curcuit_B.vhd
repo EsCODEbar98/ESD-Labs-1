627b8567378eed10447f005c4e9815c7ab32cbe4
Mariavittoria-oropallo <62213006+Mariavittoria-oropallo@users.noreply.github.com>
2020-03-28 20:24:09 +0100

    correzione HEX1

--- Es_3/curcuit_B.vhd
+++ Es_3/curcuit_B.vhd
@@ -5,7 +5,7 @@ use ieee.numeric_std.all;
 
 Entity circuit_B is
   PORT (  z : in STD_LOGIC;
-          HEXA1 : out STD_LOGIC_VECTOR(0 TO 6)
+          HEX1 : out STD_LOGIC_VECTOR(0 TO 6)
         );
 end circuit_B;
 
@@ -15,6 +15,6 @@ Architecture behaviuor of circuit_B is
 begin
   
   MUX: entity work.mux2to1_gen generic map (7)
-                  	 	 	        port map("0000001", "1111001", z, HEXA1);
+                  	 	 	        port map("0000001", "1111001", z, HEX1);
                   	 	 	          
 end architecture;
