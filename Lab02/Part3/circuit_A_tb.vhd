45a20c37e1bf3ef512b177eec1f2565c9c571a58
Mariavittoria-oropallo <62213006+Mariavittoria-oropallo@users.noreply.github.com>
2020-03-30 11:59:04 +0200

    component correction

--- Lab02/Part3/circuit_A_tb.vhd
+++ Lab02/Part3/circuit_A_tb.vhd
@@ -11,6 +11,10 @@ Architecture struct of circuit_A_tb is
   signal input_number : UNSIGNED (2 downto 0);
   signal u_dut : UNSIGNED (2 downto 0); 
   
+  component circuit_A
+     PORT (v : IN UNSIGNED(2 DOWNTO 0);
+           u : OUT UNSIGNED(2 DOWNTO 0));
+   end component;
 
 begin
   process
@@ -29,6 +33,6 @@ begin
        WAIT FOR 20 ns;
   end process; 
   
-  CIRCUIT_A: entity work.circuit_A port map( input_number, u_dut );
+  CIRC_A: circuit_A port map( input_number, u_dut );
   
 end architecture;
