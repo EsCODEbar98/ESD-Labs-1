05f6d784291bdd2cce5bea0d79c244597477bf41
Mariavittoria-oropallo <62213006+Mariavittoria-oropallo@users.noreply.github.com>
2020-03-30 12:35:35 +0200

    upload without synth.vhd

--- Lab02/Part1/decoder.vhd
+++ Lab02/Part1/decoder.vhd
@@ -3,12 +3,15 @@ use ieee.std_logic_1164.all;
 
 entity decoder is
   port ( c : in std_logic_vector(2 downto 0);
+        SW : out std_logic_vector(2 downto 0);
         HEX0 : out std_logic_vector(0 to 6));
 end decoder;
 
 architecture behavior of decoder is
   begin
+    
   -- boolean functions
+  --the light switches on when the driven value is '0'
   HEX0(0) <= not(c(0)) or c(2);
   HEX0(1) <= c(2) or (c(0) and not(c(1))) or (not(c(0)) and c(1));
   HEX0(2) <= c(2) or (c(0) and not(c(1))) or (not(c(0)) and c(1)) ;
@@ -16,7 +19,9 @@ architecture behavior of decoder is
   HEX0(4) <= c(2);
   HEX0(5) <= c(2);
   HEX0(6) <= (c(1) or c(2));
-
-  --the light switches on when the driven value is '0'
+  
+  --synth
+  SW <= c;
+  
 
 end behavior;
