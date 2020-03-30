425c065b683b9c6d8f2f25381d8723e584a394b1
Mariavittoria-oropallo <62213006+Mariavittoria-oropallo@users.noreply.github.com>
2020-03-30 11:56:09 +0200

    components correction

--- Lab02/Part3/curcuit_B.vhd
+++ Lab02/Part3/curcuit_B.vhd
@@ -12,9 +12,16 @@ end circuit_B;
 
 Architecture behaviuor of circuit_B is
   
+  component mux2to1_gen
+    generic ( dw : positive := 1 );     
+    port (x,y : in std_logic_vector ( dw - 1 downto 0 );	
+          s : in std_logic;	
+          m : out std_logic_vector ( dw -1 downto 0 ));
+  end component;
+  
 begin
   
-  MUX: entity work.mux2to1_gen generic map (7)
+  MUX: mux2to1_gen generic map (7)
                   	 	 	        port map("0000001", "1111001", z, HEX1);
                   	 	 	          
 end architecture;
