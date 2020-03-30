44ac3bd6c13f49126f6a227a9953906a069e6fab
Mariavittoria-oropallo <62213006+Mariavittoria-oropallo@users.noreply.github.com>
2020-03-30 14:33:50 +0200

    component correction

--- Lab02/Part1/decoder_tb.vhd
+++ Lab02/Part1/decoder_tb.vhd
@@ -9,11 +9,16 @@ architecture test of decoder_tb is
   signal sel : std_logic_vector(2 downto 0);
   signal output : std_logic_vector(0 to 6);
   signal out_ref : string(2 downto 1);
+
+  component decoder
+    port( sW : in std_logic_vector(2 downto 0);
+         HEX0 : out std_logic_vector(0 to 6));
+   end component;
   
   begin
     --assigned values to c2,c1,c0
     sel <= "000", "001" after 10 ns, "010" after 20 ns, "011" after 30 ns, "100" after 40 ns;
-    DUT: entity work.decoder port map (c => sel, HEX0 => output);
+    DUT: decoder port map (SW => sel, HEX0 => output);
     
     --expected output
     with sel select 
