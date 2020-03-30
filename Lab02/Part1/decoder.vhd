d08e7438b059037cd3a360e75a134215228f9462
Mariavittoria-oropallo <62213006+Mariavittoria-oropallo@users.noreply.github.com>
2020-03-30 14:30:59 +0200

    upload withouth synth.vhd

--- Lab02/Part1/decoder.vhd
+++ Lab02/Part1/decoder.vhd
@@ -2,8 +2,7 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity decoder is
-  port ( c : in std_logic_vector(2 downto 0);
-        SW : out std_logic_vector(2 downto 0);
+  port ( SW : in std_logic_vector(2 downto 0); --SW is connected to the input c
         HEX0 : out std_logic_vector(0 to 6));
 end decoder;
 
@@ -12,16 +11,13 @@ architecture behavior of decoder is
     
   -- boolean functions
   --the light switches on when the driven value is '0'
-  HEX0(0) <= not(c(0)) or c(2);
-  HEX0(1) <= c(2) or (c(0) and not(c(1))) or (not(c(0)) and c(1));
-  HEX0(2) <= c(2) or (c(0) and not(c(1))) or (not(c(0)) and c(1)) ;
-  HEX0(3) <= c(2) or (not(c(0) or c(1)));
-  HEX0(4) <= c(2);
-  HEX0(5) <= c(2);
-  HEX0(6) <= (c(1) or c(2));
-  
-  --synth
-  SW <= c;
+  HEX0(0) <= not(SW(0)) or SW(2);
+  HEX0(1) <= SW(2) or (SW(0) and not(SW(1))) or (not(SW(0)) and SW(1));
+  HEX0(2) <= SW(2) or (SW(0) and not(SW(1))) or (not(SW(0)) and SW(1)) ;
+  HEX0(3) <= SW(2) or (not(SW(0) or SW(1)));
+  HEX0(4) <= SW(2);
+  HEX0(5) <= SW(2);
+  HEX0(6) <= (SW(1) or SW(2));
   
 
 end behavior;
