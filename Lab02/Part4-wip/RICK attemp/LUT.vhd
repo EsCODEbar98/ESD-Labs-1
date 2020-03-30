25baa3fc5ad956ad370d44584bac1ccf2d2be121
Riki-code98 <62204561+Riki-code98@users.noreply.github.com>
2020-03-30 19:06:01 +0200

    Update+TB

--- Lab02/Part4-wip/RICK attemp/LUT.vhd
+++ Lab02/Part4-wip/RICK attemp/LUT.vhd
@@ -10,8 +10,11 @@ architecture behavior of LUT is
 --In pratica ho sintetizzato una LUT che mi fa il lavoro
 --di adder e shifter
 begin
-u(0)<= x(0) or (x(1)  and  x(3)) or (x(1)  and  x(2));
-u(1)<= (x(0) and x(3)) or (x(0) and x(2)) or (x(1) and not(x(2)) and not(x(3)));
-u(2)<= (not(x(0))and not(x(1)) and x(2)) or (x(2) and x(3)) or (x(0) and x(1));
-u(3)<= (not(x(0)) and not(x(1)) and x(3)) or (x(0) and not(x(3))) or (x(1) and x(2) and not(x(3)));
+u(3)<= x(3) or (x(2)  and  x(0)) or (x(2)  and  x(1));
+u(2)<= (x(3) and x(0)) or (x(3) and x(1)) or (x(2) and not(x(1)) and not(x(0)));
+u(1) <=( x(1) and x(0)) or ( not(x(3))  and not(x(2))  and x(1)) or (x(3) and not(x(1)) and not(x(0))) ;
+u(0)<= ( x(3) and  not(x(0))) or (not(x(3)) and not(x(2)) and x(0)) or (x(2) and x(1) and not(x(0)));
+
+
+
 end behavior;
